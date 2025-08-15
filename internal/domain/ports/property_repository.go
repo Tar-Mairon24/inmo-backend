12323d52b896a1a46ca012a10190c5e3606eaa4e
Tar_Mairon24 <marioirigoyen24@outlook.com>
2025-08-14 21:35:00 -0600

    feat: Property repository crud

--- internal/domain/ports/property_repository.go
+++ internal/domain/ports/property_repository.go
@@ -1,2 +1,11 @@
 package ports
 
+import "inmo-backend/internal/domain/models"
+
+type PropertyRepository interface {
+	GetAll() ([]models.PropertyResponse, error)
+	GetByID(id uint) (*models.PropertyResponse, error)
+	Create(property *models.Property) (*models.PropertyResponse, error)
+	Update(property *models.Property) (*models.PropertyResponse, error)
+	Delete(id uint) error
+}
