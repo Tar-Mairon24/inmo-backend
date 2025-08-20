6ecc0567b9bd2daab2950640b8989fd9e1304bc3
Tar_Mairon24 <marioirigoyen24@outlook.com>
2025-08-20 10:17:41 -0600

    feat: jwt service port

---
+++ internal/domain/ports/jwt_service.go
@@ -0,0 +1,9 @@
+package ports
+
+import "inmo-backend/internal/domain/models"
+
+type JWTService interface {
+    GenerateToken(user *models.User) (string, error)
+    ValidateToken(tokenString string) (*models.JWTClaims, error)
+    RefreshToken(tokenString string) (string, error)
+}
