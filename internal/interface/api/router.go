5211f54b845d94263168fdf60002d60c40ef775a
Tar_Mairon24 <marioirigoyen24@outlook.com>
2025-08-20 19:38:02 -0600

    feat: added cors for the urls of the frontend

--- internal/interface/api/router.go
+++ internal/interface/api/router.go
@@ -2,12 +2,21 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 
 	"inmo-backend/cmd/di"
 )
 
 func SetupRouter(handlers *di.Handlers, services di.Services) *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:5174"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	v1 := r.Group("/api/v1")
 	{
